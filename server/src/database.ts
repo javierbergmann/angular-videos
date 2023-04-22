a6482d6f12a510cd7012178c87110aa394657681
javierbergmann <javier.bergmann@gmail.com>
2023-04-22 08:20:04 -0300

    version fazt

--- server/src/database.ts
+++ server/src/database.ts
@@ -5,7 +5,7 @@ const pool = mysql.createPool(keys.database);
 pool.getConnection()
     .then(connection =>{
         pool.releaseConnection(connection);
-        console.log("DB is connected.!");
+        console.log("DB is connected....!");
     });
 
 export default pool;
